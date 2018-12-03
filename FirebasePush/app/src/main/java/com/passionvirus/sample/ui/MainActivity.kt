8f37d9aee09638ca8ac87e18452bbf92d7756324
sys <sys@smartstudy.co.kr>
2018-12-03 18:06:54 +0900

    Remove not use import

--- FirebasePush/app/src/main/java/com/passionvirus/sample/ui/MainActivity.kt
+++ FirebasePush/app/src/main/java/com/passionvirus/sample/ui/MainActivity.kt
@@ -2,7 +2,6 @@ package com.passionvirus.sample.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.passionvirus.firebasepush.R
 import com.passionvirus.sample.R
 
 class MainActivity : AppCompatActivity() {
