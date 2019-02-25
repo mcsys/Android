1c00bb59b54aee9223dc30ceabecf7ba718856f6
mcsys <realmcsys@gmail.com>
2019-02-26 02:07:05 +0900

    Test Calculator

---
+++ JUnitSample/app/src/main/java/com/passionvirus/junitsample/Calculator.kt
@@ -0,0 +1,21 @@
+package com.passionvirus.junitsample
+
+class Calculator {
+    companion object {
+        fun add(a : Int, b : Int) : Int {
+            return a + b
+        }
+
+        fun subtract(a : Int, b : Int) : Int {
+            return a - b
+        }
+
+        fun multiply(a : Int, b : Int) : Int {
+            return a * b
+        }
+
+        fun division(a : Int, b : Int) : Int {
+            return a / b
+        }
+    }
+}
---
+++ JUnitSample/app/src/test/java/com/passionvirus/junitsample/CalculatorTest.kt
@@ -0,0 +1,44 @@
+package com.passionvirus.junitsample
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class CalculatorTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun add(){
+        assertEquals(10, Calculator.add(2, 8))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun substract() {
+        assertEquals(0, Calculator.subtract(10, 10))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun multiply() {
+        assertEquals(0, Calculator.multiply(0, 10))
+        assertEquals(10, Calculator.multiply(1, 10))
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun division() {
+        assertEquals(2.toFloat(), Calculator.division(5, 2).toFloat(), 0.toFloat())
+        assertEquals(2, Calculator.division(4, 2))
+    }
+}
