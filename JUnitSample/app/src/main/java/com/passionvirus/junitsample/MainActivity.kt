fe28cc50cfdf24291510f703889d43c9f33c123a
mcsys <realmcsys@gmail.com>
2019-02-27 00:33:37 +0900

    Add LeakCanary

--- JUnitSample/app/src/main/java/com/passionvirus/junitsample/MainActivity.kt
+++ JUnitSample/app/src/main/java/com/passionvirus/junitsample/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.passionvirus.junitsample
 
-import android.support.v7.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        lateinit var mAcitivity : Activity
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mAcitivity = this
+
+    }
+
+    fun onButtonClicked() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 }
