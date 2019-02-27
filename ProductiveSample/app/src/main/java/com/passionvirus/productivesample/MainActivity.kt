4a5c6a1085f0e4df7acadc77d1357081468704a3
sys <sys@smartstudy.co.kr>
2019-02-27 18:50:21 +0900

    Add Glide

--- ProductiveSample/app/src/main/java/com/passionvirus/productivesample/MainActivity.kt
+++ ProductiveSample/app/src/main/java/com/passionvirus/productivesample/MainActivity.kt
@@ -4,6 +4,8 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ImageView
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,7 +17,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mAcitivity = this
+        // LeakCanary
+//        mAcitivity = this
+
+        val imageView = findViewById<ImageView>(R.id.photo)
+        Glide.with(this)
+//            .load("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
+            .load(R.drawable.leak_canary_icon)
+            .into(imageView)
 
     }
 
