ae8e0cb899f8c24f7686f1ed38a966363a7b839b
mcsys <realmcsys@gmail.com>
2019-03-01 00:03:14 +0900

    Add gson

--- ProductiveSample/app/src/main/java/com/passionvirus/productivesample/MainActivity.kt
+++ ProductiveSample/app/src/main/java/com/passionvirus/productivesample/MainActivity.kt
@@ -4,11 +4,13 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.ImageView
 import com.bumptech.glide.Glide
+import com.google.gson.Gson
 
 class MainActivity : AppCompatActivity() {
-
+    private val TAG = MainActivity::class.java.simpleName
     companion object {
         lateinit var mAcitivity : Activity
     }
@@ -20,11 +22,23 @@ class MainActivity : AppCompatActivity() {
         // LeakCanary
 //        mAcitivity = this
 
-        val imageView = findViewById<ImageView>(R.id.photo)
-        Glide.with(this)
-//            .load("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
-            .load(R.drawable.leak_canary_icon)
-            .into(imageView)
+        // Glide
+//        val imageView = findViewById<ImageView>(R.id.photo)
+//        Glide.with(this)
+////            .load("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")
+//            .load(R.drawable.leak_canary_icon)
+//            .into(imageView)
+
+        // Gson
+        val gson = Gson()
+        val user = User("mm@mm.com", "maemong")
+        val json = gson.toJson(user)
+        Log.d(TAG, json.toString())
+
+        val str = gson.fromJson("\"hello\"", String::class.java)
+        Log.d(TAG, "String $str")
+        val newUser = gson.fromJson("{\"email\":\"mm@mm.com\",\"fullname\":\"mongmong\"}", User::class.java)
+        Log.d(TAG, "${newUser.email} | ${newUser.fullname}")
 
     }
 
---
+++ ProductiveSample/app/src/main/java/com/passionvirus/productivesample/User.kt
@@ -0,0 +1,11 @@
+package com.passionvirus.productivesample
+
+class User(email : String, fullname : String) {
+    var email = ""
+    var fullname = ""
+
+    init {
+        this.email = email
+        this.fullname = fullname
+    }
+}
