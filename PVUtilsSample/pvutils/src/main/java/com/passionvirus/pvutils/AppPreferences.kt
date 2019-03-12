72eb683d34b22ad39936e9a19737c0e4961de58d
mcsys <realmcsys@gmail.com>
2019-03-12 15:39:21 +0900

    Change androidx module

--- PVUtilsSample/pvutils/src/main/java/com/passionvirus/pvutils/AppPreferences.kt
+++ PVUtilsSample/pvutils/src/main/java/com/passionvirus/pvutils/AppPreferences.kt
@@ -2,8 +2,8 @@ package com.passionvirus.pvutils
 
 import android.content.Context
 import android.content.SharedPreferences
-import android.support.annotation.NonNull
-import android.support.annotation.Nullable
+import androidx.annotation.NonNull
+import androidx.annotation.Nullable
 
 class AppPreferences {
     companion object {
