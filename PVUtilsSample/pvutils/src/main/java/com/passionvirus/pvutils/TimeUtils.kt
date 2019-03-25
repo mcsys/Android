27840712f09e5ce9db3da280480f5023c0f8764a
mcsys <realmcsys@gmail.com>
2019-03-25 16:57:02 +0900

    Add Time features

--- PVUtilsSample/pvutils/src/main/java/com/passionvirus/pvutils/TimeUtils.kt
+++ PVUtilsSample/pvutils/src/main/java/com/passionvirus/pvutils/TimeUtils.kt
@@ -13,6 +13,71 @@ class TimeUtils {
         lateinit var dateFormat : String
         lateinit var dateLocale : Locale
         lateinit var dateTimezone : TimeZone
+        val calendar = Calendar.getInstance()
+
+        fun getDateTimeInMillis() : Long {
+            return Calendar.getInstance().timeInMillis
+        }
+
+        fun getCustomDateTimeInMillis() : Long {
+            return calendar.timeInMillis
+        }
+
+        fun setCalendarYear(year : Int)  {
+            calendar.set(Calendar.YEAR, year)
+        }
+
+        fun addCalendarYear(year : Int)  {
+            calendar.add(Calendar.YEAR, year)
+        }
+
+        fun setCalendarMonth(month : Int)  {
+            calendar.set(Calendar.MONTH, month)
+        }
+
+        fun addCalendarMonth(month : Int)  {
+            calendar.add(Calendar.MONTH, month)
+        }
+
+        fun setCalendarDay(day : Int) {
+            calendar.set(Calendar.DAY_OF_YEAR, day)
+        }
+
+        fun addCalendarDay(day : Int) {
+            calendar.add(Calendar.DAY_OF_YEAR, day)
+        }
+
+        fun setCalendarHour(hour : Int) {
+            calendar.set(Calendar.HOUR_OF_DAY, hour)
+        }
+
+        fun addCalendarHour(hour : Int) {
+            calendar.add(Calendar.HOUR_OF_DAY, hour)
+        }
+
+        fun setCalendarMinute(minute : Int) {
+            calendar.set(Calendar.MINUTE, minute)
+        }
+
+        fun addCalendarMinute(minute : Int) {
+            calendar.add(Calendar.MINUTE, minute)
+        }
+
+        fun setCalendarSecond(second : Int) {
+            calendar.set(Calendar.SECOND, second)
+        }
+
+        fun addCalendarSecond(second : Int) {
+            calendar.add(Calendar.SECOND, second)
+        }
+
+        fun getDate(timeInMillis : Long) : Date {
+            return Date(timeInMillis)
+        }
+
+        fun getCustomDate() : Date {
+            return Date(calendar.timeInMillis)
+        }
 
         fun getDateTime() : String {
             if (!::dateFormat.isInitialized) {
@@ -60,9 +125,15 @@ class TimeUtils {
             return sdf.format(date)
         }
 
+        fun isBefore(date1 : Date, date2 : Date) : Boolean{
+            return date1.before(date2)
+        }
 
-
-
+        fun isBefore(timeInMillis1: Long, timeInMillis2: Long) : Boolean{
+            val date1 = getDate(timeInMillis1)
+            val date2 = getDate(timeInMillis2)
+            return date1.before(date2)
+        }
 
     }
 }
