982090aba3ee32b4da1df32036d7ef5b8b529f2d
mcsys <realmcsys@gmail.com>
2019-04-24 18:38:41 +0900

    Binding view-viewmodel

--- TddMvvm/app/src/main/java/com/passionvirus/sample/tddmvvm/ui/MainFragment.kt
+++ TddMvvm/app/src/main/java/com/passionvirus/sample/tddmvvm/ui/MainFragment.kt
@@ -7,16 +7,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.passionvirus.sample.tddmvvm.BR
 import com.passionvirus.sample.tddmvvm.R
 import com.passionvirus.sample.tddmvvm.databinding.FragmentMainBinding
+import com.passionvirus.sample.tddmvvm.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
     val TAG = MainFragment::class.java.simpleName
 
     lateinit var binding : FragmentMainBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val viewModel = MainViewModel()
+
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false)
+        binding.setVariable(BR.vm, viewModel)
 
         return binding.root
     }
