902b50314415ef051175501350bd4c947238c59f
mcsys <realmcsys@gmail.com>
2019-06-14 18:30:35 +0900

    update coroutine sample

--- ProductiveSample/app/src/main/java/com/passionvirus/productivesample/CoroutineActivity.kt
+++ ProductiveSample/app/src/main/java/com/passionvirus/productivesample/CoroutineActivity.kt
@@ -6,30 +6,156 @@ import android.util.Log
 import kotlinx.coroutines.*
 
 class CoroutineActivity: AppCompatActivity() {
+    val TAG = CoroutineActivity::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine)
 
+/***
+ * 아래 사이트를 참고합니다.
+ * https://tourspace.tistory.com/151?category=797357
+ * 
+ * runBlocking 내부 코드가 완료되기 전에 다음을 수행하지 않는다.
+ * job 을 join 한 경우, 은 모두 완료되기 전에 다음을 수행하지 않는다.
+ * coroutineScope : 내부 로직이 모두 수행되기전까지 다음라인이 수행되지 않는다.
+ *
+ *
+ *
+ *
+***/
 
         /*
+        Log.d(TAG, "Start")
         GlobalScope.launch { // launch a new coroutine in background and continue
             delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-            Log.d("TEST1234", "World!") // print after delay
+            Log.d(TAG, "World!") // print after delay
         }
-        Log.d("TEST1234", "Hello") // main thread continues while coroutine is delayed
+        Log.d(TAG, "Hello") // main thread continues while coroutine is delayed
         Thread.sleep(2000L) // block main thread for 2 seconds to keep JVM alive
+        Log.d(TAG, "End")
         */
 
         /*
-        GlobalScope.launch { // launch a new coroutine in background and continue
-            delay(1000L)
-            Log.d("TEST1234", "World!")
+        runBlocking<Unit> {
+            Log.d(TAG, "Start")
+            GlobalScope.launch {
+                delay(1000L)
+                Log.d(TAG, "World!")
+            }
+            Log.d(TAG, "Hello")
+            Thread.sleep(2000L)
+            Log.d(TAG, "End")
         }
-        Log.d("TEST1234", "Hello") // main thread continues here immediately
-        runBlocking {     // but this expression blocks the main thread
-            delay(2000L)  // ... while we delay for 2 seconds to keep JVM alive
+        Log.d(TAG, "End funtion")
+        */
+
+        /*
+        runBlocking {
+            Log.d(TAG, "Start")
+            val job = GlobalScope.launch {
+                delay(1000L)
+                Log.d(TAG, "World!")
+            }
+            Log.d(TAG, "Hello")
+            job.join()
+            Log.d(TAG, "End")
         }
         */
+
+        /*
+        runBlocking {
+            val jobs = List(10) {
+                launch {
+                    delay(1000L)
+                    Log.d(TAG, "Do Job")
+                }
+            }
+//            jobs.forEach { it.join() }
+            Log.d(TAG, "End runBlock")
+        }
+        Log.d(TAG, "End funtion")
+        */
+
+        /*
+        runBlocking {
+            launch {
+                delay(700L)
+                Log.d(TAG, "Block 1")
+            }
+            Log.d(TAG, "Go coroutineScope")
+            coroutineScope {
+                launch {
+                    delay(500L)
+                    Log.d(TAG, "Block 2")
+                }
+                delay(100L)
+                Log.d(TAG, "End coroutineScope")
+            }
+            Log.d(TAG, "End funtion")
+        }
+        */
+
+        /*
+        runBlocking {
+            launch {
+                doWorld()
+            }
+            Log.d(TAG, "Hello")
+        }
+        */
+
+        /*
+        runBlocking {
+            GlobalScope.launch {
+                repeat(1000) { i ->
+                    Log.d(TAG, "I'm sleep : $i")
+                    delay(500L)
+                }
+            }
+            delay(1300L)
+            Log.d(TAG, "End funtion")
+        }
+        */
+
+        runBlocking {
+            val job = launch {
+                repeat(1000) { i ->
+                    Log.d(TAG, "I'm sleep : $i")
+                    delay(500L)
+                }
+            }
+            delay(1300L)
+            Log.d(TAG, "Cancel And Join")
+            job.cancel()
+            job.join()
+            Log.d(TAG, "End Done")
+        }
+
+
+
+
+
+
+
+
+
+
+
+        /*
+                runBlocking {
+                    Log.d(TAG, "Start")
+                    GlobalScope.launch { // launch a new coroutine in background and continue
+                        delay(1000L)
+                        Log.d(TAG, "World!")
+                    }
+                    Log.d(TAG, "Hello") // main thread continues here immediately
+                    runBlocking {     // but this expression blocks the main thread
+                        delay(2000L)  // ... while we delay for 2 seconds to keep JVM alive
+                    }
+                    Log.d(TAG, "End")
+                }
+                */
 
         /*
         runBlocking {
@@ -206,6 +332,7 @@ class CoroutineActivity: AppCompatActivity() {
         }
         */
 
+        /*
         runBlocking {
             val result = withTimeoutOrNull(1300L) {
                 repeat(1000) { i ->
@@ -216,12 +343,13 @@ class CoroutineActivity: AppCompatActivity() {
             }
             Log.d("TEST1234", "Result is $result")
         }
+        */
+
 
     }
 
-
     private suspend fun doWorld() {
         delay(1000L)
-        Log.d("TEST1234", "Hello")
+        Log.d(TAG, "World")
     }
 }
