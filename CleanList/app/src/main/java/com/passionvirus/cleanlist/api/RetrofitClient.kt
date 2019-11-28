579a7c8ac1a923742aaf6c7540ef49fa202cc63f
mcsys <realmcsys@gmail.com>
2019-11-28 14:37:08 +0900

    Retrofit update

--- CleanList/app/src/main/java/com/passionvirus/cleanlist/api/RetrofitClient.kt
+++ CleanList/app/src/main/java/com/passionvirus/cleanlist/api/RetrofitClient.kt
@@ -1,11 +1,12 @@
 package com.passionvirus.cleanlist.api
 
+import android.util.Log
+import com.google.gson.*
+import com.passionvirus.cleanlist.api.entity.ApiEntity
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import com.google.gson.GsonBuilder
-import com.google.gson.Gson
-
+import java.lang.reflect.Type
 
 
 class RetrofitClient {
@@ -15,22 +16,33 @@ class RetrofitClient {
         @JvmStatic
         fun getClient(baseUrl: String): Retrofit {
             // Code - V2_1
-//            val gson = GsonBuilder()
-//                .registerTypeAdapter(GithubRepo::class.java, GithubRepoDeserializer())
-//                .create()
+
 
             if (!::retrofit.isInitialized) {
+                val gson = GsonBuilder()
+                    .registerTypeAdapter(ApiEntity.AbilityList::class.java, AbilityListDeserializer())
+                    .create()
+
                 retrofit = Retrofit.Builder()
                     .baseUrl(baseUrl)
-                    .addConverterFactory(GsonConverterFactory.create())
+//                    .addConverterFactory(GsonConverterFactory.create())
                     // Code - V1
-//                    .addConverterFactory(GsonConverterFactory.create(gson))
+                    .addConverterFactory(GsonConverterFactory.create(gson))
                     // Code - V2
                     .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                     .build()
             }
 
             return retrofit
+        }
+    }
+
+    private class AbilityListDeserializer : JsonDeserializer<ApiEntity.AbilityList> {
+        //@Throws(JsonParseException::class)
+        override fun deserialize(json: JsonElement?, type: Type?, context: JsonDeserializationContext?): ApiEntity.AbilityList {
+            val entity = ApiEntity.AbilityList()
+
+            return entity
         }
     }
 }
